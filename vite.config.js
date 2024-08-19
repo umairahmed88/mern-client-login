0fdefc469ea2b43b00fcecad76f6949398956d77
umairahmed88 <umairahmed_88@ymail.com>
2024-08-19 15:37:25 +0500

    updated the vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
+		port: 3000,
+		host: true,
 		proxy: {
 			"/api": {
 				target: "https://mern-api-login-production.up.railway.app",
