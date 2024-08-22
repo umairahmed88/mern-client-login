6286861607d0e4e80f94dbb4d94fa0914fec60e1
umairahmed88 <umairahmed_88@ymail.com>
2024-08-22 14:32:20 +0500

    Update Vercel configurations

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
 	root: "src",
 	build: {
-		outDir: "./dist",
+		outDir: "../dist",
 		emptyOutDir: true,
 	},
 	server: {
