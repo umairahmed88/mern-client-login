56d07774dc9ee8dada0ab370cd4b7241b657a880
umairahmed88 <umairahmed_88@ymail.com>
2024-08-22 13:24:49 +0500

    moved index.html to src directory

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+	root: "src",
+	build: {
+		outDir: "../dist",
+		emptyOutDir: true,
+	},
 	server: {
 		proxy: {
 			"/api": {
 				target: "https://ua-mern-api-login.vercel.app",
 				changeOrigin: true,
-				secure: false, // Optional: Set to true if your Vercel URL uses HTTPS
+				secure: false,
 			},
 		},
 	},
