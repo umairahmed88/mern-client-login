cc14465eb8e3b00d03b12d3cc9400ee91bb9a706
umairahmed88 <umairahmed_88@ymail.com>
2024-08-23 23:43:32 +0500

    updated vercel and vite config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		outDir: "dist",
-		rollupOptions: {
-			input: "src/index.html",
-		},
-	},
-	base: "./",
 	server: {
 		proxy: {
 			"/api": {
@@ -19,4 +11,5 @@ export default defineConfig({
 			},
 		},
 	},
+	plugins: [react()],
 });
