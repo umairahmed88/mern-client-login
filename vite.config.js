d0acf17083e211a6e62941834bb342b9ebe728ef
umairahmed88 <umairahmed_88@ymail.com>
2024-08-24 01:41:41 +0500

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+	server: {
+		proxy: {
+			"/api": {
+				target: "https://ua-mern-api-login.vercel.app",
+				changeOrigin: true,
+				secure: true, // Use true if your backend uses HTTPS
+			},
+		},
+	},
 	plugins: [react()],
 });
