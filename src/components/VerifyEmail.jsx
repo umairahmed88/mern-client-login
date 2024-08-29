443a22f7a53f599938923e0eb051a1e21df60a13
umairahmed88 <umairahmed_88@ymail.com>
2024-08-30 02:01:12 +0500

    updated email verify component

--- src/components/VerifyEmail.jsx
+++ src/components/VerifyEmail.jsx
@@ -13,21 +13,29 @@ const VerifyEmail = () => {
 				const response = await axios.get(
 					`https://ua-mern-api.vercel.app/api/v1/auth/verify-email?token=${token}`
 				);
+
 				if (response.data.message === "Email verified successfully") {
 					navigate("/signin");
 				} else {
-					console.error(
-						"Verification failed:",
-						response && response.data.message
-					);
+					console.error("Verification failed:", response.data.message);
 				}
 
 				console.log("Token received:", token);
 			} catch (error) {
-				console.error(
-					"Error verifying email:",
-					error.response ? error.response.data : error.message
-				);
+				// Log detailed error information
+				if (error.response) {
+					// Error from the server
+					console.error(
+						"Server-side error verifying email:",
+						error.response.data.message
+					);
+				} else if (error.request) {
+					// No response was received
+					console.error("No response received:", error.request);
+				} else {
+					// Other errors
+					console.error("Error verifying email:", error.message);
+				}
 			}
 		};
 
