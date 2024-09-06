5d3e1083c1021272a359e8bface786481482e80a
umairahmed88 <umairahmed_88@ymail.com>
2024-09-06 14:34:32 +0500

    updated and remove avatar error

--- src/pages/auth/Profile.jsx
+++ src/pages/auth/Profile.jsx
@@ -114,7 +114,10 @@ const Profile = () => {
 			<div className=''>
 				<div className=' flex justify-center items-center'>
 					<img
-						src={currentUser?.sanitizedUser?.avatar}
+						src={
+							currentUser?.sanitizedUser?.avatar ||
+							"https://media.istockphoto.com/id/1451587807/vector/user-profile-icon-vector-avatar-or-person-icon-profile-picture-portrait-symbol-vector.jpg?s=612x612&w=0&k=20&c=yDJ4ITX1cHMh25Lt1vI1zBn2cAKKAlByHBvPJ8gEiIg="
+						}
 						alt='profile image'
 						className='rounded-full h-28 w-28 object-contain'
 					/>
