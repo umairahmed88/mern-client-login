67ec841509708de13bc757923703794047bd5a57
umairahmed88 <umairahmed_88@ymail.com>
2024-11-17 19:49:31 +0500

    updated auth slices

--- src/redux/auth/authSlices.js
+++ src/redux/auth/authSlices.js
@@ -255,7 +255,6 @@ const authSlice = createSlice({
 			})
 			.addCase(updateUser.fulfilled, (state, action) => {
 				state.loading = false;
-				state.currentUser = action.payload || action.payload.auth;
 				state.message = action.payload.message;
 			})
 			.addCase(updateUser.rejected, (state, action) => {
